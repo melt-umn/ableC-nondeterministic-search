81c89647fda77f0f26819a6c6f08c5741d96f307
Lucas Kramer <krame505@umn.edu>
2018-03-21 01:25:48 -0400

    Properly handle dangling-finally ambiguity

--- grammars/edu.umn.cs.melt.exts.ableC.search/concretesyntax/SearchStmt.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.search/concretesyntax/SearchStmt.sv
@@ -4,8 +4,11 @@ terminal Choice_t  'choice'  lexer classes {Ckeyword};
 terminal Succeed_t 'succeed' lexer classes {Ckeyword};
 terminal Fail_t    'fail'    lexer classes {Ckeyword};
 terminal Choose_t  'choose'  lexer classes {Ckeyword};
-terminal Finally_t 'finally' lexer classes {Ckeyword};
+terminal Finally_t 'finally' lexer classes {Ckeyword}, precedence = 2;
 terminal Require_t 'require' lexer classes {Ckeyword};
+
+-- For 'dangling-finally' resolution
+terminal Prec1RCurly_t /}/ precedence = 1, action { context = tail(context); };
 
 {-
  - For double-brace-enclosed host statments
@@ -42,10 +45,27 @@ concrete productions top::SearchStmt_c
   { top.ast = failSearchStmt(location=top.location); }
 | '{' ss::SearchStmts_c '}'
   { top.ast = compoundSearchStmt(foldSeqSearchStmt(ss.ast), location=top.location); }
-| css::ChoiceSearchStmt_c
-  { top.ast = css.ast; }
-| css::ChoiceSearchStmt_c 'finally' '{' b::BlockItemList_c '}'
-  { top.ast = finallySearchStmt(css.ast, foldStmt(b.ast), location=top.location); }
+-- Optional 'finally' clause can't be factored out, unfourtunately, due to use of precedence
+| 'choice' '{' ss::SearchStmts_c Prec1RCurly_t
+  { top.ast = compoundSearchStmt(foldChoiceSearchStmt(ss.ast), location=top.location); }
+| 'choice' '{' ss::SearchStmts_c Prec1RCurly_t 'finally' '{' b::BlockItemList_c '}'
+  { top.ast = finallySearchStmt(compoundSearchStmt(foldChoiceSearchStmt(ss.ast), location=top.location), foldStmt(b.ast), location=top.location); }
+| 'choice' 'for' '(' init::ExprStmt_c cond::ExprStmt_c iter::Expr_c ')' body::SearchStmt_c
+  { top.ast = choiceForSearchStmt(init.asMaybeExpr, cond.asMaybeExpr, justExpr(iter.ast), body.ast, location=top.location); }
+| 'choice' 'for' '(' init::ExprStmt_c cond::ExprStmt_c iter::Expr_c ')' body::SearchStmt_c 'finally' '{' b::BlockItemList_c '}'
+  { top.ast = finallySearchStmt(choiceForSearchStmt(init.asMaybeExpr, cond.asMaybeExpr, justExpr(iter.ast), body.ast, location=top.location), foldStmt(b.ast), location=top.location); }
+| 'choice' 'for' '(' init::ExprStmt_c cond::ExprStmt_c ')' body::SearchStmt_c
+  { top.ast = choiceForSearchStmt(init.asMaybeExpr, cond.asMaybeExpr, nothingExpr(), body.ast, location=top.location); }
+| 'choice' 'for' '(' init::ExprStmt_c cond::ExprStmt_c ')' body::SearchStmt_c 'finally' '{' b::BlockItemList_c '}'
+  { top.ast = finallySearchStmt(choiceForSearchStmt(init.asMaybeExpr, cond.asMaybeExpr, nothingExpr(), body.ast, location=top.location), foldStmt(b.ast), location=top.location); }
+| 'choice' 'for' '(' init::Declaration_c cond::ExprStmt_c iter::Expr_c ')' body::SearchStmt_c
+  { top.ast = choiceForDeclSearchStmt(init.ast, cond.asMaybeExpr, justExpr(iter.ast), body.ast, location=top.location); }
+| 'choice' 'for' '(' init::Declaration_c cond::ExprStmt_c iter::Expr_c ')' body::SearchStmt_c 'finally' '{' b::BlockItemList_c '}'
+  { top.ast = finallySearchStmt(choiceForDeclSearchStmt(init.ast, cond.asMaybeExpr, justExpr(iter.ast), body.ast, location=top.location), foldStmt(b.ast), location=top.location); }
+| 'choice' 'for' '(' init::Declaration_c cond::ExprStmt_c ')' body::SearchStmt_c
+  { top.ast = choiceForDeclSearchStmt(init.ast, cond.asMaybeExpr, nothingExpr(), body.ast, location=top.location); }
+| 'choice' 'for' '(' init::Declaration_c cond::ExprStmt_c ')' body::SearchStmt_c 'finally' '{' b::BlockItemList_c '}'
+  { top.ast = finallySearchStmt(choiceForDeclSearchStmt(init.ast, cond.asMaybeExpr, nothingExpr(), body.ast, location=top.location), foldStmt(b.ast), location=top.location); }
 | 'if' '(' cond::Expr_c ')' tc::SearchStmt_c
   { top.ast = ifThenSearchStmt(cond.ast, tc.ast, location=top.location); }
 | 'if' '(' cond::Expr_c ')' tc::SearchStmt_c 'else' ec::SearchStmt_c 
@@ -90,43 +110,6 @@ concrete productions top::SearchStmt_c
   { top.ast = chooseSucceedSearchStmt(fromId(f), foldExpr(args.ast), location=top.location); }
 | 'require' c::Expr_c ';'
   { top.ast = requireSearchStmt(c.ast, location=top.location); }
-
-nonterminal ChoiceSearchStmt_c with ast<SearchStmt>, location;
-concrete productions top::ChoiceSearchStmt_c
-| 'choice' '{' ss::SearchStmts_c '}'
-  { top.ast = compoundSearchStmt(foldChoiceSearchStmt(ss.ast), location=top.location); }
-| 'choice' 'for' '(' init::ExprStmt_c cond::ExprStmt_c iter::Expr_c ')' '{' body::SearchStmts_c '}'
-  {
-    top.ast =
-      choiceForSearchStmt(
-        init.asMaybeExpr, cond.asMaybeExpr, justExpr(iter.ast),
-        compoundSearchStmt(foldChoiceSearchStmt(body.ast), location=top.location),
-        location=top.location);
-  }
-| 'choice' 'for' '(' init::ExprStmt_c cond::ExprStmt_c ')' '{' body::SearchStmts_c '}'
-  {
-    top.ast =
-      choiceForSearchStmt(
-        init.asMaybeExpr, cond.asMaybeExpr, nothingExpr(),
-        compoundSearchStmt(foldChoiceSearchStmt(body.ast), location=top.location),
-        location=top.location);
-  }
-| 'choice' 'for' '(' init::Declaration_c cond::ExprStmt_c iter::Expr_c ')' '{' body::SearchStmts_c '}'
-  {
-    top.ast =
-      choiceForDeclSearchStmt(
-        init.ast, cond.asMaybeExpr, justExpr(iter.ast),
-        compoundSearchStmt(foldChoiceSearchStmt(body.ast), location=top.location),
-        location=top.location);
-  }
-| 'choice' 'for' '(' init::Declaration_c cond::ExprStmt_c ')' '{' body::SearchStmts_c '}'
-  {
-    top.ast =
-      choiceForDeclSearchStmt(
-        init.ast, cond.asMaybeExpr, nothingExpr(),
-        compoundSearchStmt(foldChoiceSearchStmt(body.ast), location=top.location),
-        location=top.location);
-  }
 
 -- Mirrors Declaration_c, needed to avoid failing MDA by spilling follow set
 closed nonterminal SearchDeclaration_c with location, ast<Decl>;
