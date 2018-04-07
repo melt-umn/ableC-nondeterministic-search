ef9ae7c72b4bc7ae76b817bd94c7071b654d0b9b
Lucas Kramer <krame505@umn.edu>
2018-04-07 17:30:27 -0400

    Add missing include guard

--- include/pick.h
+++ include/pick.h
@@ -1,6 +1,9 @@
 #include <pthread.h>
 #include <refcount.h>
 
+#ifndef _PICK_H
+#define _PICK_H
+
 typedef struct pick_status_s *pick_status_t;
 struct pick_status_s {
   pthread_mutex_t mutex;
@@ -9,3 +12,5 @@ struct pick_status_s {
 
 refcount_tag_t pick_status_init(pick_status_t *p_status);
 _Bool try_pick(pick_status_t status);
+
+#endif
